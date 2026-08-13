5d0154ae2fa3865f69d38046089d880c4a637cff
shaumikalam <shaumik.alam@gmail.com>
2026-08-13 16:56:38 +0000

    Remove old telconnect folder (renamed to TelConnect Call Data Pipeline)

--- telconnect/summarize_gold.sql
+++
@@ -1,7 +0,0 @@
--- summarize_gold: Aggregate network summary by region and network type
--- Job: TelConnect CDR Pipeline > summarize_gold task
--- Warehouse: 8c717a440d022dc0
-
-SELECT region, network_type, total_calls, drop_rate_pct
-FROM telconnect_lab.gold.network_summary
-ORDER BY drop_rate_pct DESC;
